ea0b49778a50a50c62f6c3bf3a7702449dc5ab30
Artem <artemklishch@gmail.com>
2019-11-25 21:31:51 +0200

    task5

--- lesson4/task5/index.js
+++ lesson4/task5/index.js
@@ -1,15 +1,15 @@
 const m = 10;
 const n = 20;
-let result, res = 0;
+let result = 0;
 for(let i = m; i <= n; i++){
     if(i % 2 === 0 && i % 4 !== 0 && i % 5 !== 0){
-        res += i;
+        result += i;
     }else if(i % 3 === 0 && i % 5 !== 0){
-        res -= i;
+        result -= i;
     }else if(i % 4 === 0 && i % 5 !== 0){
-        res *= i;
+        result *= i;
     }else if(i % 5 === 0){
         console.log(i);
     }else continue;
 }
-result = res;
+console.log(result);
