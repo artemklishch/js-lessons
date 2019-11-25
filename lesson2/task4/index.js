7f7db46c2e2f7fae8bf56510f08814dd3ba41f29
Artem <artemklishch@gmail.com>
2019-11-25 12:10:29 +0200

    task4

---
+++ lesson2/task4/index.js
@@ -0,0 +1,8 @@
+const a = 4;
+const b = 2;
+console.log('a: ', a);
+console.log('a: [', a, ']');
+console.log('a - b = ', a - b);
+console.log('a + b = ', a + b);
+console.log('a / b = ', a / b);
+console.log('a * b = ', a * b);
