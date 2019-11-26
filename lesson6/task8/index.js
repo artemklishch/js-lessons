8e43de84089803fc51a8f1a52a2d72e93c43febf
Artem <artemklishch@gmail.com>
2019-11-26 22:27:57 +0200

    task8

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,18 +1,33 @@
+// function uniqueCount(array){
+//     let arr = [];
+//     for(let i = array.length-1; i >= 0; i--){
+//         for(let j = 0; j < i; j++){
+//             let temp = array[i];
+//             let calc = 0;
+//             if(arr.length !== 0 && array[j] === temp){
+//                 for(let f = 0; f < arr.length; f++){
+//                     if(arr[f] === temp){
+//                         calc++;
+//                     }
+//                 }
+//             }   
+//             if(calc === 0 && array[j] === temp) arr.push(array[j]);
+//         }
+//     }
+//     return arr;
+// }
+
 function uniqueCount(array){
-    let arr = [];
+    let number = 0;
+    let num = 0;
     for(let i = array.length-1; i >= 0; i--){
         for(let j = 0; j < i; j++){
-            let temp = array[i];
-            let calc = 0;
-            if(arr.length !== 0 && array[j] === temp){
-                for(let f = 0; f < arr.length; f++){
-                    if(arr[f] === temp){
-                        calc++;
-                    }
-                }
-            }   
-            if(calc === 0 && array[j] === temp) arr.push(array[j]);
+            if(array[i] === array[j]){
+                num = array[i];
+                number++;
+            } 
         }
     }
-    return arr;
+    return num + ": " + number;
 }
+console.log(uniqueCount([8, 1,2, 12,13,5,5,6,7, 5,]));
