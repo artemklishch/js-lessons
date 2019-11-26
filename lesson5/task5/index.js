fd8009f89090f66c56f55fb4bd942f52e107d491
Artem <artemklishch@gmail.com>
2019-11-26 14:06:08 +0200

    task5

--- lesson5/task5/index.js
+++ lesson5/task5/index.js
@@ -1,6 +1,6 @@
 function getSum(firstNum, endNum){
     let sum = 0;
-    for(let i = firstNum; i < endNum; i++){
+    for(let i = firstNum; i <= endNum; i++){
         if(i % 2 === 0) sum += i;
     }
     return sum;
