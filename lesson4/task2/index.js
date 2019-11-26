9200aefa98a4890f8aceb4045f4804872383eba7
Artem <artemklishch@gmail.com>
2019-11-26 12:28:16 +0200

    task2

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,7 +1,6 @@
 const m = 10;
 const n = 20;
-let result = 0, multRes = 1;
+let result = 1;
 for(let i = 0; m <= n; i++){
-    if(m % 2 === 1) multRes *= m;
+    if(m % 2 === 1) result *= m;
 }
-result = multRes;
