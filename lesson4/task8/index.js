8a8b69baf6b079c096c09d6c66bce7ae3b8e1db9
Artem <artemklishch@gmail.com>
2019-11-26 11:48:44 +0200

    task8

--- lesson4/task8/index.js
+++ lesson4/task8/index.js
@@ -2,4 +2,6 @@ let sum = 0;
 for(let i = 0; i <= 1000; i++){
     sum += i;
 }
-console.log(sum/1234 > 0 || false);
+let numberAfterDevision = sum/1234;
+let reminder = numberAfterDevision % 1234; 
+console.log(numberAfterDevision > reminder || false);
