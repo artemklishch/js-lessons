787f730d0c8910b04c65a876da01808bbaad5672
Artem <artemklishch@gmail.com>
2019-11-26 10:42:45 +0200

    task8

--- lesson4/task8/index.js
+++ lesson4/task8/index.js
@@ -1,9 +1,5 @@
 let sum = 0;
-let wholePart, reminder, result;
 for(let i = 0; i <= 1000; i++){
     sum += i;
 }
-wholePart = Math.trunc(sum/1234);
-reminder = sum/1234 - wholePart;
-result = wholePart > reminder || false;
-console.log(result);
+console.log(sum/1234 > 0 || false);
