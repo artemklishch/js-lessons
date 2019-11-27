c3f3fe5a32ca1fbf6091f5de57692041e6814b80
Artem <artemklishch@gmail.com>
2019-11-27 21:37:49 +0200

    task4

--- lesson9/task4/index.js
+++ lesson9/task4/index.js
@@ -6,23 +6,3 @@ const getPeople = obj => Object.entries(obj)
     .map(elem => elem = elem[1])
     .flat()
     .map(elem => elem.name);
-
-    // //const obj = {};
-    // const rooms = {
-    //     room1: [
-    //         { name: 'room1 name1' },
-    //         { name: 'room1 name2' },
-    //         { name: 'room1 name3' },
-    //         { name: 'room1 name4' },
-    //     ],
-    //     room2: [
-    //         { name: 'room2 name1' },
-    //     ],
-    //     room3: [
-    //         { name: 'room3 name1' },
-    //         { name: 'room3 name2' },
-    //         { name: 'room3 name3' },
-    //     ],
-    // };
-    // //console.log(getPeople(obj));
-    // console.log(getPeople(rooms));
