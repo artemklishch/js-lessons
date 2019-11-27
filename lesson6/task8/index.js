f21930d3e683932db0e74f97dec059421a2246b1
Artem <artemklishch@gmail.com>
2019-11-27 10:00:47 +0200

    task8

--- lesson6/task8/index.js
+++ lesson6/task8/index.js
@@ -1,31 +1,8 @@
-// function uniqueCount(array){
-//     let arr = [];
-//     for(let i = array.length-1; i >= 0; i--){
-//         for(let j = 0; j < i; j++){
-//             let temp = array[i];
-//             let calc = 0;
-//             if(arr.length !== 0 && array[j] === temp){
-//                 for(let f = 0; f < arr.length; f++){
-//                     if(arr[f] === temp){
-//                         calc++;
-//                     }
-//                 }
-//             }   
-//             if(calc === 0 && array[j] === temp) arr.push(array[j]);
-//         }
-//     }
-//     return arr;
-// }
-
-function uniqueCount(array){
-    let number = 0;
-    for(let i = array.length-1; i >= 0; i--){
-        for(let j = 0; j < i; j++){
-            if(array[i] === array[j]){
-                number++;
-            } 
-        }
+function uniqueCount(arr){
+    let calc = 0;
+    arr.sort((a,b) => a - b);
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== arr[i+1]) calc++;
     }
-    return number;
+    return calc;
 }
-// console.log(uniqueCount([8, 1,2, 12,13,5,5,6,7, 5,]));
