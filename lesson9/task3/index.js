0b9110f428450920f9ce746c030f37de8f6fff3c
Artem <artemklishch@gmail.com>
2019-11-27 19:06:20 +0200

    task3

--- lesson9/task3/index.js
+++ lesson9/task3/index.js
@@ -1,8 +1,3 @@
-const getCustomersList = obj => {
-    let previousArrayOfObjects = Object.entries(obj)
-        .map(elem => {
-            elem[1].id = elem[0];
-            return elem[1];
-        });
-    return previousArrayOfObjects.sort((a, b) => a.age > b.age ? 1 : -1);
-};
+const getCustomersList = obj => Object.entries(obj)
+        .map(elem => elem = {...elem[1], id:elem[0]})
+        .sort((a, b) => a.age > b.age ? 1 : -1);
