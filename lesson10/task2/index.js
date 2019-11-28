bbe9aea94eb89c88000cf02d8694b10faa9f227a
Artem <artemklishch@gmail.com>
2019-11-28 14:42:05 +0200

    task2

--- lesson10/task2/index.js
+++ lesson10/task2/index.js
@@ -9,8 +9,3 @@ const getParsedFloats = arr => arr
 
 const getParsedFloatsV2 = arr => arr
     .reduce((acc,elem) => acc.concat(parseFloat(elem)),[]); 
-
-
-
-// const arr = [3.5, 45.12,'12px', '12.45', '34.5px' ];
-// console.log(getParsedFloatsV2(arr));
