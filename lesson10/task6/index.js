e27054247f95bad2b715bbaa1ad673d6a6a4c0c8
Artem <artemklishch@gmail.com>
2019-11-28 18:56:35 +0200

    task6

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -1,11 +1,11 @@
 const getRandomNumbers = (length, min, max) => {
     if(max < min) return null;
+    if(max - min < 1) return null;
     min = Math.ceil(min);
     max = Math.floor(max);
     let arr=[];
     for(let i = min; i < length; i++){
         let temp = (Math.floor(Math.random() * (max - min)) + min);
-        if(!Number.isInteger(temp)) return null;
         arr.push(temp);
     }
     return arr;
