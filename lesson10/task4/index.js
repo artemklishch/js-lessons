026d23a43e390e14539e0c356806aa8a153f42ab
Artem <artemklishch@gmail.com>
2019-11-28 15:23:18 +0200

    task4

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,6 +1,3 @@
 const getMaxAbsoluteNumber = arr => 
     (!Array.isArray(arr) || arr.length === 0) ? null : 
     Math.max(...arr.map(num => Math.abs(num)));
-
-// const arr = [-777, 3, -1, 45, -20];
-// console.log(getMaxAbsoluteNumber(arr));
