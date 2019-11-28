78e97b72f205f5e672a05bd738a5a6b6033585d1
Artem <artemklishch@gmail.com>
2019-11-28 17:37:59 +0200

    task6

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -1,15 +1,10 @@
 const getRandomNumbers = (length, start, end) => {
-    if(end < start) return null;
     let arr = [];
     for(let i = 0; i < length; i++){
-        arr[i] = Math.floor(Math.random() * (end - start)) + start;
+        arr[i] = Math.random() * (end - start) + start;
     }
-    for(let num of arr){
-        if(!Number.isInteger(num)) return null;
-    }
-    
     return arr;
 };
 
 
-// console.log(getRandomNumbers(2, 2, 10));
+// console.log(getRandomNumbers(100, -0.1, 0.1));
