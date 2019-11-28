17fb4baeaa251ef6ff50a56e5201eaf0e6fe656d
Artem <artemklishch@gmail.com>
2019-11-28 14:46:40 +0200

    task3

---
+++ lesson10/task3/index.js
@@ -0,0 +1,5 @@
+const multiRound = num => 
+[Math.round(num), Math.floor(num), Math.ceil(num), Math.trunc(num), +num.toFixed(2)];
+
+// const number = 13.52226666;
+// console.log(multiRound(number));
