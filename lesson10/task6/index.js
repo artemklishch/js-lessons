dba5494d45424338fe4cdb8350f455868e304c00
Artem <artemklishch@gmail.com>
2019-11-28 18:49:51 +0200

    task6

--- lesson10/task6/index.js
+++ lesson10/task6/index.js
@@ -1,21 +1,14 @@
-const getRandomNumbers = (length, start, end) => {
-    if(end < start) return null;
-    if(end - start < 1) return null;
-    if(Number.isInteger(end)){
-        end -= 0.1;
-        end = Math.floor(end);
-    }else end = Math.floor(end);
-    if(Number.isInteger(start)){
-        start += 0.1;
-        start = Math.ceil(start);
-    }else start = Math.ceil(start);
-    let arr = [];
-    for(let i = 0; i < length; i++){
-        arr[i] = Math.round(Math.random() * (end - start)) + start;
+const getRandomNumbers = (length, min, max) => {
+    if(max < min) return null;
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    let arr=[];
+    for(let i = min; i < length; i++){
+        let temp = (Math.floor(Math.random() * (max - min)) + min);
+        arr.push(temp);
     }
-    
     return arr;
 };
 
 
-//console.log(getRandomNumbers(10, 5.1, 10.5));
+// console.log(getRandomNumbers(100, -0.1, 0.1));
