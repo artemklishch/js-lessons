275c775d899c26290528e8c90e3df32c0ae0b56a
Artem <artemklishch@gmail.com>
2019-11-28 12:52:30 +0200

    task1

--- lesson10/task1/index.js
+++ lesson10/task1/index.js
@@ -8,14 +8,14 @@ const getNaN = arr => arr
 .filter(elem => Number.isNaN(elem));
 
 const getNaNV2 = arr => arr
-    .filter(elem => isNaN(elem))
-    .filter(elem => typeof elem === 'number');
+    .filter(elem => typeof elem === 'number')
+    .filter(elem => isNaN(elem));
 
 const getIntegers = arr => arr
     .filter(elem => Number.isInteger(elem));
 
 
 
-// const arr = ['a',NaN, 17,18,19.5,'text', NaN];
-// console.log(getNaNV2(arr));
-// console.log(arr);
+const arr = ['a',NaN, 17,18,19.5,'text', NaN];
+console.log(getNaNV2(arr));
+console.log(arr);
