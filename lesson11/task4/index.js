f27097a015f0b64c1be270be702b97a423de54a0
Artem <artemklishch@gmail.com>
2019-11-29 18:30:36 +0200

    task4

--- lesson11/task4/index.js
+++ lesson11/task4/index.js
@@ -1,14 +1,3 @@
-// const countOccurrences = (string, substring) => {
-//     if(substring.length === 0) return null;
-//     let calc = 0;
-//     let position = 0;
-//     while(string.indexOf(substring, position) !== -1){
-//         position += string.indexOf(substring, position);
-//         calc++;
-//     }
-//     return calc;
-// };
-
 const countOccurrences = (string, substring) => {
     if(substring.length === 0) return null;
     let calc = 0;
@@ -18,8 +7,3 @@ const countOccurrences = (string, substring) => {
     }
     return calc;
 };
-
-
-// const str = "My friend, hello, buddy, hello!";
-// const sub_str = "hello";
-// console.log(countOccurrences(str, sub_str));
