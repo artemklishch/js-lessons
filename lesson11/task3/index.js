acc545b9030919c4f1507003e5f99adf80f2ef44
Artem <artemklishch@gmail.com>
2019-11-29 16:52:49 +0200

    task3

--- lesson11/task3/index.js
+++ lesson11/task3/index.js
@@ -1,32 +1,3 @@
-// const splitString = (str, len) => {
-//     if(typeof str !== 'string') return null;
-//     let arr = [];
-//     let startPosition = 0;
-// 	while(true){
-// 		let chunk = str.substr(startPosition,len); 
-//         if(chunk.length >= len){
-//             arr.push(chunk);
-// 		    startPosition += len;   
-//         }
-//         if(!len){
-//             let tempStr = ".";
-//             for(let i = chunk.length; i < 9; i++){
-//                 chunk += tempStr;
-//             }
-//             arr.push(chunk + tempStr);
-// 		    break;
-//         }
-//         if(chunk.length < len){
-//             let tempStr = "";
-//             for(let i = 0; i < len-1; i++){
-//                 tempStr += "."
-//             }
-//             arr.push(chunk + tempStr);
-// 		    break;
-//         } 
-// 	}
-//     return arr;
-// };
 const splitString = (str, len = 10) => {
     if (typeof str !== 'string') return null;
     let arrLength = Math.ceil(str.length/len);
@@ -45,6 +16,3 @@ const splitString = (str, len = 10) => {
             }      
         });
 };
-
-// const str = 'abcd efgh';
-// console.log(splitString(str, 4));
