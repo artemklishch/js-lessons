184a0b634619864f464c134904cc9767fe316804
Artem <artemklishch@gmail.com>
2019-11-29 22:02:12 +0200

    task3

---
+++ lesson12/task3/index.js
@@ -0,0 +1,8 @@
+const cleanTransactionsLis = sums => sums
+    .map(elem => Number(elem))
+    .filter(elem => !Number.isNaN(elem))
+    .map(elem => "$" + elem.toFixed(2));
+   
+
+// const sums = [' 1.9 ', '16.4', 17, ' 1 dollar '];
+// console.log(cleanTransactionsLis(sums));
