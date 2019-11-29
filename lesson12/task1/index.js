381e797af8bb8ba322a4ffb45c8c006cb15c7b2c
Artem <artemklishch@gmail.com>
2019-11-29 20:50:42 +0200

    task1

---
+++ lesson12/task1/index.js
@@ -0,0 +1,12 @@
+const calc = expression => {
+	const [a, operator, b] = expression.split(" "); 
+	let result;
+	switch (operator) {
+		case "+": result = +a + +b; break;
+        case "-": result = a - b;	break;		
+        case "*": result = a * b;	break;	
+        case "/": result = a / b;	break;	
+	}
+
+	return `${expression} = ${result}`;
+}
