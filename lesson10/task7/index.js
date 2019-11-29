20d2d51a7bf87ed81368b1f53f1d9f74fba0d4b4
Artem <artemklishch@gmail.com>
2019-11-29 09:57:22 +0200

    task7

--- lesson10/task7/index.js
+++ lesson10/task7/index.js
@@ -1,8 +1,8 @@
 const superRound = (num, points) => [(Math.floor(num*Math.pow(10,points)))/Math.pow(10,points), 
         (Math.round(num*Math.pow(10,points)))/Math.pow(10,points),
         (Math.ceil(num*Math.pow(10,points)))/Math.pow(10,points),
-        (Math.trunc(num)),
+        (Math.trunc(num*Math.pow(10,points)))/Math.pow(10,points),
         +num.toFixed(points)];
     
 
-// console.log(superRound(-2.5678971124, 2));
+console.log(superRound(-2.5678971124, 2));
