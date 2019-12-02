7364d816b7e1b88450f8315fc15929a0e0434670
Artem <artemklishch@gmail.com>
2019-12-02 14:20:37 +0200

    task1

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -5,7 +5,7 @@ export function sendMessage (name){
 	const sender = "Gromecode";
 	console.log(`${name}, ${message}! Your ${sender}`);
 }
-export function setMessage(text){
+function setMessage(text){
 	message = text;
 }
 
