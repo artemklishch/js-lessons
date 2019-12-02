7af8a93aeef5413bfa0c35639da6fcc323d51a81
Artem <artemklishch@gmail.com>
2019-12-02 15:00:28 +0200

    task1

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -2,15 +2,9 @@
 
 let message = "Just learn it!";
 export function sendMessage (name){
-	const sender = "Gromcode";
+	let sender = "Gromcode";
 	console.log(`${name}, ${message} Your ${sender}`);
 }
 export function setMessage(text){
 	message = text;
 }
-
-sendMessage("Ann");
-setMessage("Good job!");
-sendMessage("Ann");
-
-
