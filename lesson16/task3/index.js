0856b955a30f24fadc1b2f63a34361e866288d91
Artem <artemklishch@gmail.com>
2019-12-03 16:03:51 +0200

    task3

--- lesson16/task3/index.js
+++ lesson16/task3/index.js
@@ -1,5 +1,6 @@
 
 export function createArrayOfFunctions(number){
+    if(!number) return [];
     if(typeof number !== 'number') return null;
     let arr = [];
     for(let i = 0; i < number; i++){
