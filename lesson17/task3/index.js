11bc0b388d49a89a25a3ae4c8110908f681fc092
Artem <artemklishch@gmail.com>
2019-12-03 23:45:59 +0200

    task3

---
+++ lesson17/task3/index.js
@@ -0,0 +1,29 @@
+function defer(func, ms){
+    return function(){
+        setTimeout(() => func.call(this, ...arguments), ms);
+    } 
+}
+// const sayHi = () => {
+//     console.log("Hi");
+// };
+
+// const sum = (a,b) => {
+//     console.log(a+b);
+// };
+
+// const user = {
+//     name: "Tom",
+//     sayHi(){
+//         console.log(`Hi. I'm ${this.name}!`);
+//     },
+// };
+
+
+// console.log(sayHi());
+// const defferedSayHi = defer(sayHi, 2000);
+// const defferedSayHi1 = defer(sum, 2000);
+// defferedSayHi();
+// defferedSayHi1(10,10);
+
+// const defferedSayHi2 = defer(user.sayHi, 2000);
+// defferedSayHi2.call({name:"Bob"});
