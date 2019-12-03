d8201f1c3cb06dca4c1deb2f859ee2e40810382f
Artem <artemklishch@gmail.com>
2019-12-03 13:22:20 +0200

    task3

--- lesson15/task3/index.js
+++ lesson15/task3/index.js
@@ -61,31 +61,31 @@ export const createLogger = () => {
                 .sort((a,b) => a.dateTime > b.dateTime ? 1 : -1)
                 .map(elem => elem = elem.message) :
                 commonArr
-                .filter(elem => elem.type === type)
-                .sort((a,b) => a.dateTime > b.dateTime ? 1 : -1),
+                .filter(elem => elem.type === type),
+                // .sort((a,b) => a.dateTime > b.dateTime ? 1 : -1),
 
     }
 };
 
-// let message1 = createLogger();
+let message1 = createLogger();
 
 
-// message1.warn("I warn you");
-// message1.log("some log");
-// message1.warn("I wa8989rn you correctly");
-// message1.error("BIGEST ERROR EVER!");
-// message1.warn("I warn you immediatly");
-// message1.error("ERROR!");
+message1.warn("I warn you");
+message1.log("some log");
+message1.warn("I wa8989rn you correctly");
+message1.error("BIGEST ERROR EVER!");
+message1.warn("I warn you immediatly");
+message1.error("ERROR!");
 
-// const arrWarnMessage1 = message1.getRecords("warn"); 
-// console.log(arrWarnMessage1);
+const arrWarnMessage1 = message1.getRecords(); 
+console.log(arrWarnMessage1);
 
 
-// let message2 = createLogger();
-// message2.warn("I warn youyyyyyyyyyyyy");
-// message2.warn("I wa8989rn you correctyyyyyyyyyyyyyyyyyyly");
-// const arrWarnMessage2 = message2.getRecords("warn"); 
-// console.log(arrWarnMessage2);
+let message2 = createLogger();
+message2.warn("I warn youyyyyyyyyyyyy");
+message2.warn("I wa8989rn you correctyyyyyyyyyyyyyyyyyyly");
+const arrWarnMessage2 = message2.getRecords("warn"); 
+console.log(arrWarnMessage2);
 
 
 
