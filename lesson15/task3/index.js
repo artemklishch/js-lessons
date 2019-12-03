ebfe644d2a39365a1921b02ddd76a3cf959244d4
Artem <artemklishch@gmail.com>
2019-12-03 13:41:08 +0200

    task3

--- lesson15/task3/index.js
+++ lesson15/task3/index.js
@@ -1,43 +1,5 @@
 export const createLogger = () => {
     let commonArr = [];
-    // function warn(string){
-    //     return commonArr.push(
-    //         {
-    //             message: string,
-    //             dateTime:new Date(),
-    //             type: "warn",
-    //         })
-    // }
-    // function error(string){
-    //     return commonArr.push(
-    //         {
-    //             message: string,
-    //             dateTime:new Date(),
-    //             type: "error",
-    //         })
-    // }
-    // function log(string){
-    //     return commonArr.push(
-    //         {
-    //             message: string,
-    //             dateTime:new Date(),
-    //             type: "log",
-    //         })
-    // }
-    // function getRecords(type){
-    //     return !type ? commonArr
-    //     .sort((a,b) => a.dateTime > b.dateTime ? 1 : -1)
-    //     .map(elem => elem = elem.message) :
-    //     commonArr
-    //     .filter(elem => elem.type === type)
-    //     .sort((a,b) => a.dateTime > b.dateTime ? 1 : -1)
-    // }
-    // return {
-    //     warn,
-    //     error,
-    //     log,
-    //     getRecords,
-    // }
     return{
         warn: (string) => commonArr.push(
                     {
@@ -62,37 +24,6 @@ export const createLogger = () => {
                 .filter(elem => elem = elem.message) :
                 commonArr
                 .filter(elem => elem.type === type),
-                // .sort((a,b) => a.dateTime > b.dateTime ? 1 : -1),
 
     }
 };
-
-// let message1 = createLogger();
-
-
-// message1.warn("I warn you");
-// message1.log("some log");
-// message1.warn("I wa8989rn you correctly");
-// message1.error("BIGEST ERROR EVER!");
-// message1.warn("I warn you immediatly");
-// message1.error("ERROR!");
-
-// const arrWarnMessage1 = message1.getRecords(); 
-// console.log(arrWarnMessage1);
-
-
-// let message2 = createLogger();
-// message2.warn("I warn youyyyyyyyyyyyy");
-// message2.warn("I wa8989rn you correctyyyyyyyyyyyyyyyyyyly");
-// const arrWarnMessage2 = message2.getRecords("warn"); 
-// console.log(arrWarnMessage2);
-
-
-
-
-
-
-
-
-
-
