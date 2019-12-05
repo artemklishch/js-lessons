5e375b1f8cad651fe726b00810bf177c4bcd532a
Artem <artemklishch@gmail.com>
2019-12-05 11:16:53 +0200

    task6

--- lesson17/task6/index.js
+++ lesson17/task6/index.js
@@ -1,17 +1,17 @@
-export function bind(func, context){
+export function bind(func, context, ...args){
     return function(...args) {
 		return func.apply(context,	args);
 	}
 }
 
-// function sum(a,b){
-// 	return a + b;
-// }
-// const obj = {
-// 	num: 2,
-// 	sum(a,b){
-// 		return a + b;
-// 	},
-// };
-// const a = bind(sum, obj);
-// console.log(a(2,2));
+function sum(a,b){
+	return a + b;
+}
+const obj = {
+	num: 2,
+	sum(a,b){
+		return a + b;
+	},
+};
+const a = bind(sum, obj);
+console.log(a(2,2));
