2fca4ac9ca94861fd53f0c4fbc6500ac2d1f40ee
Artem <artemklishch@gmail.com>
2019-12-05 12:37:28 +0200

    task6

--- lesson17/task6/index.js
+++ lesson17/task6/index.js
@@ -1,21 +1,5 @@
-export function bind(func, context){
-    return function(...args) {
-		return func.apply(context, args);
+export function bind(func, context, ...args1){
+    return function(...args2) {
+		return func.apply(context, args2, args1);
 	}
 }
-
-// function mult(a,b){
-// 	return this.mult = a * b;
-// }
-// const obj1 = {
-// 	mult: 0,
-// };
-// const obj2 = {
-// 	mult:0,
-// };
-// const aMult = bind(mult, obj1);
-// console.log(aMult(2,2));
-// console.log(obj1);
-
-// aMult.call(obj2, 3, 3);
-// console.log(obj2, obj1)
