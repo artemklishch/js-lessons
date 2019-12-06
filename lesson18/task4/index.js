1a1ba84aed28473d43bc6dddce9427599afcf3ac
Artem <artemklishch@gmail.com>
2019-12-06 12:00:02 +0200

    task4

--- lesson18/task4/index.js
+++ lesson18/task4/index.js
@@ -6,28 +6,3 @@ export function spy(func){
     spied.calls = [];
     return spied;
 }
-// function test(a, b) {
-//     return (a + b);
-// }
-
-// const spyTest = spy(test);
-// spyTest(4, 2);
-// spyTest(9, 1);
-// console.log(spyTest.calls);
-
-
-
-// const user = {
-//     name: 'John',
-//     sayHi() {
-//         return this.name;
-//     }
-// };
-
-// const spyMethod = spy(user.sayHi);
-// spyMethod.calls = [];
-// console.log(spyMethod.apply({ name: 'Tom' })); // 'Tom'
-
-// console.log(spyMethod.calls); // [ [] ]
-
-
