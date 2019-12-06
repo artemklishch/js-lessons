3f020cb2025e469e75806fb5c4d19afcabf78c7a
Artem <artemklishch@gmail.com>
2019-12-06 16:39:14 +0200

    task3

--- lesson19/task3/index.js
+++ lesson19/task3/index.js
@@ -10,22 +10,3 @@ export default {
         this.lastName = lastName;
     },
 };
-// user.fullName = 'Tom Hardy';
-// console.log(user.fullName);
-
-// export default (firstName, lastName) => {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-// };
-
-// Object.prototype.getfullName = function(){
-//     return `${this.firstName} ${this.lastName}`;
-// };
-// Object.prototype.setfullName = function(value){
-//             let [firstName, lastName] = value.split(' ');
-//             this.firstName = firstName;
-//             this.lastName = lastName;
-//         };
-// const user1 = new User("John", "Doe");
-// user1.setfullName("Tom Hardy");
-// console.log(user1.getfullName());
