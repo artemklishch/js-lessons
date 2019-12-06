079c04d23f1926f5d7762bbdca6bce6ed762aa5e
Artem <artemklishch@gmail.com>
2019-12-06 16:38:29 +0200

    task3

--- lesson19/task3/index.js
+++ lesson19/task3/index.js
@@ -1,31 +1,31 @@
-// export const user = {
-//     firstName: 'John',
-//     lastName: 'Doe',
-//     get fullName(){
-//         return `${this.firstName} ${this.lastName}`;
-//     },
-//     set fullName(value){
-//         let [firstName, lastName] = value.split(' ');
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//     },
-// };
+export default {
+    firstName: 'John',
+    lastName: 'Doe',
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`;
+    },
+    set fullName(value){
+        let [firstName, lastName] = value.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    },
+};
 // user.fullName = 'Tom Hardy';
 // console.log(user.fullName);
 
-export default (firstName, lastName) => {
-    this.firstName = firstName;
-    this.lastName = lastName;
-};
+// export default (firstName, lastName) => {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+// };
 
-Object.prototype.getfullName = function(){
-    return `${this.firstName} ${this.lastName}`;
-};
-Object.prototype.setfullName = function(value){
-            let [firstName, lastName] = value.split(' ');
-            this.firstName = firstName;
-            this.lastName = lastName;
-        };
+// Object.prototype.getfullName = function(){
+//     return `${this.firstName} ${this.lastName}`;
+// };
+// Object.prototype.setfullName = function(value){
+//             let [firstName, lastName] = value.split(' ');
+//             this.firstName = firstName;
+//             this.lastName = lastName;
+//         };
 // const user1 = new User("John", "Doe");
 // user1.setfullName("Tom Hardy");
 // console.log(user1.getfullName());
