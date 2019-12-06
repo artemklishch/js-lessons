7040e7c73657071d68aa0cd30300eb68f316519d
Artem <artemklishch@gmail.com>
2019-12-06 16:09:42 +0200

    task2

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -3,14 +3,3 @@ export function getOwnProps(obj){
         .filter(elem => typeof elem[1] !== 'function')
         .map(elem => elem[0]);
 }
-
-const obj = {
-    name: "Tom",
-    age: 27,
-    interest: "soccer",
-    sayHi(){
-        console.log(`Hi, Iam ${this.name}`);
-    }
-};
-console.log(getOwnProps(obj));
-console.log(typeof obj.sayHi);
