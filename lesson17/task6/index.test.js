0360f3eee1676b98ee76c856ddc38b45f9e6713f
Artem <artemklishch@gmail.com>
2019-12-06 12:13:40 +0200

    task6

--- lesson17/task6/index.test.js
+++ lesson17/task6/index.test.js
@@ -1,5 +1,36 @@
 import { bind } from './index.js';
 
+
+//first test
+it("should be used instead method .bind()", () => {
+    const bruce = { name: "Bruce" };
+    function update(occupation, birthYear){
+        this.birthYear = birthYear;
+        this.occupation = occupation;
+    }
+
+    const updateBruce = bind(update, bruce, 1949);
+    updateBruce("actor");
+    const resOnlyOccupation = bruce.occupation;
+    expect(resOnlyOccupation).toEqual("actor");
+});
+
+//second test
+it("should be used instead method .bind()", () => {
+    const bruce = { name: "Bruce" };
+    const madeline = { name: "Madeline" };
+    function update(occupation, birthYear){
+        this.birthYear = birthYear;
+        this.occupation = occupation;
+    }
+
+    const updateBruce = bind(update, bruce, 1949);
+    updateBruce.call(madeline, "king", 1274); 
+    const resOnlyYear = bruce.birthYear;
+    expect(resOnlyYear).toEqual(1274);
+});
+
+//third test
 it("should be used instead method .bind()", () => {
     const bruce = { name: "Bruce" };
     const madeline = { name: "Madeline" };
@@ -10,13 +41,7 @@ it("should be used instead method .bind()", () => {
 
     const updateBruce = bind(update, bruce, 1949);
     updateBruce("actor");
-    const resOnlyOccupation = bruce.occupation;
-    expect(resOnlyOccupation).toEqual("actor");
-
     updateBruce.call(madeline, "king", 1274); 
-    const resOnlyYear = bruce.birthYear;
-    expect(resOnlyYear).toEqual(1274);
     const resNewOccupation = bruce.occupation;
     expect(resNewOccupation).toEqual("king");
-
 });
