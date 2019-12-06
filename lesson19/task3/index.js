db9b01104c781ce208085b1600aa81e4073ef5ec
Artem <artemklishch@gmail.com>
2019-12-06 16:18:33 +0200

    task3

---
+++ lesson19/task3/index.js
@@ -0,0 +1,12 @@
+export const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`;
+    },
+    set fullName(value){
+        let [firstName, lastName] = value.split(' ');
+        this.firstName = firstName;
+        this.lastName = lastName;
+    },
+};
