9353475b2d1af94768b48fc5df4ad4d7c2323756
Artem <artemklishch@gmail.com>
2019-12-06 17:16:20 +0200

    task4

---
+++ lesson19/task4/index.js
@@ -0,0 +1,26 @@
+export function User(name, age){
+    this.name = name;
+    this.age = age;
+}
+User.prototype.sayHi = function(){
+    console.log(`Hi, I am ${this.name}`);
+};
+User.prototype.requestNewPhoto = function(){
+    console.log(`New photo request was sent for ${this.name}`);
+};
+User.prototype.setAge = function(age){  
+    if(age < 0){
+        return false;
+    }
+    if(age >= 0){
+        this.age = age;
+        if(this.age >= 25) console.log(`New photo request was sent for ${this.name}`);
+        return age;
+    }
+};
+
+// const user1 = new User("John", 21);
+// user1.sayHi();
+// user1.requestNewPhoto();
+// user1.setAge(-26);
+// console.log(user1.setAge(-16));
