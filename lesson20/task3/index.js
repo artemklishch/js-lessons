68259c11c0918904368a997c83243ce2c429d741
Artem <artemklishch@gmail.com>
2019-12-07 21:53:46 +0200

    task3

---
+++ lesson20/task3/index.js
@@ -0,0 +1,23 @@
+export class Wallet{
+    _balance = 0;
+
+    getBalance(){
+        return this._balance;
+    }
+    deposit(amount){
+        this._balance += amount;
+    }
+    withdraw(sum){
+        if(sum > this._balance) {
+            console.log(`No enough funds`);
+            return;
+        }
+        this._balance -= sum;
+    }
+}
+// const balance1 = new Wallet();
+// balance1.deposit(100);
+// balance1.withdraw(10);
+// balance1.withdraw(110);
+// console.log(balance1.getBalance());
+// console.log(balance1);
