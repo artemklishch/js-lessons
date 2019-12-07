6c607a5cf62e5c82fb3077a81d85a90b0587ae3e
Artem <artemklishch@gmail.com>
2019-12-07 20:51:32 +0200

    task2

---
+++ lesson20/task2/index.js
@@ -0,0 +1,31 @@
+export class Vehicle {
+    constructor(name, numberOfWheels) {
+        this.name = name;
+        this.numberOfWheels = numberOfWheels;
+    }
+    move() {
+        console.log(`${this.name} is moving`);
+    }
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+}
+export class Ship extends Vehicle {
+    constructor(name, maxSpeed) {
+        super(name, false);
+        this.maxSpeed = maxSpeed;
+    }
+    move() {
+        console.log(`${this.name} lifting anchor up`);
+        super.move();
+    }
+    stop() {
+        super.stop();
+        console.log(`${this.name} lifting anchor down`);
+    }
+}
+
+// const ship1 = new Ship("Argo", 20);
+// console.log(ship1);
+// ship1.move();
+// ship1.stop();
