80360d7d7ccba1c6d8e1fbf7c2c54028fc7c74bf
Artem <artemklishch@gmail.com>
2019-12-07 22:27:56 +0200

    task4

---
+++ lesson20/task4/index.js
@@ -0,0 +1,28 @@
+class Order{
+    isConfirmed = false;
+    constructor(price, city, type){
+        this.price = price;
+        this.city = city;
+        this.type = type;
+        this.id = Math.random().toString();
+        this.dateCreated = new Date();
+    }
+    checkPrice(){
+        return this.price > 1000 ? true : false;
+    }
+    confirmOrder(){
+        this.isConfirmed = true;
+        this.dateConfirmed = new Date();
+    }
+    isValidType(){
+        if(this.type === "Buy" || this.type === "Sell"){
+            return true;
+        }else return false;
+    }
+}
+
+// const order1 = new Order(1500, "Kharkiv", "By");
+// console.log(order1);
+// console.log(order1.checkPrice());
+// console.log(order1.confirmOrder());
+// console.log(order1.isValidType());
