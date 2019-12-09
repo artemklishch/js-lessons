81c05c1c469782dc2171a23481d516d0ed7d2b09
Artem <artemklishch@gmail.com>
2019-12-09 10:57:46 +0200

    task5

--- lesson20/task5/index.js
+++ lesson20/task5/index.js
@@ -23,7 +23,7 @@ export class UserRepository{
     }
     getUserNames(){
         return this.usersArray
-            .map(elem => elem.name);
+            .map(elem => elem.userName);
     }
     getUserIds(){
         return this.usersArray
