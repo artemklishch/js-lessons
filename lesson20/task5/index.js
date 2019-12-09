38a29cfd2e0cc61a645383a79fd9c2a14c2a09d8
Artem <artemklishch@gmail.com>
2019-12-09 11:57:42 +0200

    task5

--- lesson20/task5/index.js
+++ lesson20/task5/index.js
@@ -18,9 +18,6 @@ export class UserRepository{
     constructor(users){
         this._users = Object.freeze(users);
     }
-    get getUsersArray(){
-        return this._users;
-    }
     getUserNames(){
         return this.getUsersArray
             .map(elem => elem.getUserName);
@@ -34,17 +31,20 @@ export class UserRepository{
             .filter(elem => elem.getId === id)
             .map(elem => elem.getUserName)[0];
     }
+    get getUsersArray(){
+        return this._users;
+    }
 }
 
-const user1 = new User("11100000000", "Tomasso Campanella", "session 1");
-const user2 = new User("11777777777", "Leonardo", "session 2");
-let arr = [user1, user2];
-const arrayOfUsers = new UserRepository(arr);
-console.log(arrayOfUsers);
-console.log(arrayOfUsers.getUserNames());
-console.log(arrayOfUsers.getUserIds());
-console.log(arrayOfUsers.getUserNameById("11100000000"));
-console.log(user1.getId);
+// const user1 = new User("11100000000", "Tomasso Campanella", "session 1");
+// const user2 = new User("11777777777", "Leonardo", "session 2");
+// let arr = [user1, user2];
+// const arrayOfUsers = new UserRepository(arr);
+// console.log(arrayOfUsers);
+// console.log(arrayOfUsers.getUserNames());
+// console.log(arrayOfUsers.getUserIds());
+// console.log(arrayOfUsers.getUserNameById("11100000000"));
+// console.log(user1.getId);
 
 
 
