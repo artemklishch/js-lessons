2717765361e257c92981ed50bdb29206d23f6468
Artem <artemklishch@gmail.com>
2019-12-09 10:37:04 +0200

    task5

--- lesson20/task5/index.js
+++ lesson20/task5/index.js
@@ -1,13 +1,25 @@
 export class User{
     constructor(id, name, sessionId){
-        this.id = id;
-        this.name = name;
-        this.sessionId = sessionId;
+        this._id = id;
+        this._name = name;
+        this._sessionId = sessionId;
+    }
+    get id(){
+        return this._id;
+    }
+    get name(){
+        return this._name;
+    }
+    get sessionId(){
+        return this._sessionId;
     }
 }
 export class UserRepository{
     constructor(users){
-        this.users = Object.freeze(users);
+        this._users = Object.freeze(users);
+    }
+    get users(){
+        return this._users;
     }
     getUserNames(){
         return this.users
@@ -32,6 +44,7 @@ export class UserRepository{
 // console.log(arrayOfUsers.getUserNames());
 // console.log(arrayOfUsers.getUserIds());
 // console.log(arrayOfUsers.getUserNameById("11100000000"));
+// console.log(user1.id);
 
 
 
