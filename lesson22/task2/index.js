987a3a77ff870be0b0dafc36bef537336e3158a3
Artem <artemklishch@gmail.com>
2019-12-11 13:07:41 +0200

    task2

--- lesson22/task2/index.js
+++ lesson22/task2/index.js
@@ -1,62 +1,39 @@
-// const tasks = [
-//     { text: 'Buy milk', done: false },
-//     { text: 'Pick up Tom from airport', done: false },
-//     { text: 'Visit party', done: false },
-//     { text: 'Visit doctor', done: true },
-//     { text: 'Buy meat', done: true },
-// ];
-// const renderListItems = listItems => {
-//     const listElem = document.querySelector('.list');
+//start code
+const tasks = [
+    { text: 'Buy milk', done: false },
+    { text: 'Pick up Tom from airport', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
+    { text: 'Buy meat', done: true },
+];
+const renderListItems = listItems => {
+    const listElem = document.querySelector('.list');
 
-//     const listItemsElems = listItems
-//         .sort((a, b) => b.done - a.done)
-//         .map(({ text, done }) => {
-//             const listItemElem = document.createElement('li');
-//             listItemElem.classList.add('list__item');
-//             if (done) {
-//                 listItemElem.classList.add('list__item_done');
-//             }
-//             const checkboxElem = document.createElement('input');
-//             checkboxElem.setAttribute('type', 'checkbox');
-//             checkboxElem.checked = done;
-//             checkboxElem.classList.add('list__item-checkbox');
-//             listItemElem.append(checkboxElem, text);
-//             return listItemElem;
-//         });
-//     listElem.append(...listItemsElems);
-// };
-// renderListItems(tasks);
-
-
-
-
-// const checkBoxes = document.querySelectorAll('.list__item-checkbox');
-// for(let i = 0; i < checkBoxes.length; i++){
-//     checkBoxes[i].onchange = func;
-// }
-function func(){
-    const allListItems = document.querySelectorAll('.list__item');
-    const listItemElem = document.querySelector('.list');
-    const closest = this.closest('.list__item');
-    closest.classList.toggle('list__item_done');
-    if(closest.done === false){
-        closest.done = true;
-    }else closest.done = false;   
-    setTimeout(()=> {
-        let newList = [...allListItems].sort((a, b) => b.done > a.done ? 1 : -1);
-        newList.forEach(elem => listItemElem.append(elem));
-    },500);
-}
-
-
-
-
+    const listItemsElems = listItems
+        .sort((a, b) => a.done - b.done)
+        .map(({ text, done }) => {
+            const listItemElem = document.createElement('li');
+            listItemElem.classList.add('list__item');
+            if (done) {
+                listItemElem.classList.add('list__item_done');
+            }
+            const checkboxElem = document.createElement('input');
+            checkboxElem.setAttribute('type', 'checkbox');
+            checkboxElem.checked = done;
+            checkboxElem.onchange = func;
+            checkboxElem.classList.add('list__item-checkbox');
+            listItemElem.append(checkboxElem, text);
+            return listItemElem;
+        });
+    listElem.append(...listItemsElems);
+};
+renderListItems(tasks);
 
 
 const taskInput = document.querySelector('.task-input');
 const createTaskBtn = document.querySelector('.create-task-btn');
 const renderAdditionalListItems = addotionalListItem => {
-    const listItemElem = document.querySelector('.list');
+    const listElem = document.querySelector('.list');
     const newItem = document.createElement('li');
     newItem.classList.add('list__item');
     newItem.done = false;
@@ -65,12 +42,77 @@ const renderAdditionalListItems = addotionalListItem => {
     checkboxElem.classList.add('list__item-checkbox');
     checkboxElem.onchange = func;
     newItem.append(checkboxElem, addotionalListItem);
-    listItemElem.append(newItem);
+    listElem.prepend(newItem);
     taskInput.value = '';
+    tasks.push(newItem);
 };
 createTaskBtn.addEventListener('click', function () {
     if (taskInput.value !== "") return renderAdditionalListItems(taskInput.value);
 });
+
+
+function func() {
+    const closest = this.closest('li');
+    closest.classList.toggle('list__item_done');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //next code
+// const taskInput = document.querySelector('.task-input');
+// const createTaskBtn = document.querySelector('.create-task-btn');
+// const renderAdditionalListItems = addotionalListItem => {
+//     const listItemElem = document.querySelector('.list');
+//     const newItem = document.createElement('li');
+//     newItem.classList.add('list__item');
+//     newItem.done = false;
+//     const checkboxElem = document.createElement('input');
+//     checkboxElem.setAttribute('type', 'checkbox');
+//     checkboxElem.classList.add('list__item-checkbox');
+//     checkboxElem.onchange = func;
+//     newItem.append(checkboxElem, addotionalListItem);
+//     listItemElem.append(newItem);
+//     taskInput.value = '';
+// };
+// createTaskBtn.addEventListener('click', function () {
+//     if (taskInput.value !== "") return renderAdditionalListItems(taskInput.value);
+// });
+
+
+
+// function func(){
+//     const allListItems = document.querySelectorAll('.list__item');
+//     const listItemElem = document.querySelector('.list');
+//     const closest = this.closest('.list__item');
+//     closest.classList.toggle('list__item_done');
+//     if(closest.done === false){
+//         closest.done = true;
+//     }else closest.done = false;   
+//     setTimeout(()=> {
+//         let newList = [...allListItems].sort((a, b) => b.done > a.done ? 1 : -1);
+//         newList.forEach(elem => listItemElem.append(elem));
+//     },500);
+// }
+
+
+
+
+
+
+
 
 
 
