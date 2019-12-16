c16463ae3ec9b84cd6f00104e8830eb5a31fd647
Artem <artemklishch@gmail.com>
2019-12-16 14:06:26 +0200

    task3

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -1,28 +1,9 @@
 export const getDiff = (startDate, endDate) => {
-    // const deff = Math.abs(endDate - startDate);
-    // const days = Math.trunc(deff/1000/60/60/24);
-    // const hours = Math.trunc(((deff/1000/60/60/24) - days)*60);
-    // const minutes = Math.trunc(((((deff/1000/60/60/24) - days)*60) - hours)*60);
-    // const seconds = Math.trunc(((((((deff/1000/60/60/24) - days)*60) - hours)*60) - minutes)*100);
-    // return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-    // const deff = Math.abs(endDate - startDate);
-    // const days = deff/1000/60/60/24;
-    // const hours = ((days - Math.trunc(days))*60);
-    // const minutes = ((hours - Math.trunc(hours))*60);
-    // const seconds = Math.trunc((minutes - Math.trunc(minutes))*100);
-    // return `${Math.trunc(days)}d ${Math.trunc(hours)}h ${Math.trunc(minutes)}m ${Math.trunc(seconds)}s`;
-
-
-    const deff = Math.abs(endDate - startDate);
-    const days = Math.trunc(deff/1000/60/60/24);
-    let tempDeffVal = deff/1000/60/60/24;
-    const hours = Math.trunc((tempDeffVal - days)*60);
-    tempDeffVal = (tempDeffVal - days)*60;
-    const minutes = Math.trunc((tempDeffVal - hours)*60);
-    tempDeffVal = (tempDeffVal - hours)*60;
-    const seconds = Math.trunc(( tempDeffVal - minutes)*100);
+    const diff = Math.abs(endDate - startDate);
+    const days = parseInt(diff/1000/60/60/24);
+    const hours = parseInt((diff/1000/60/60/24 - days)*24);
+    const minutes = parseInt(((diff/1000/60/60/24 - days)*24 - hours)*60);
+    const seconds = parseInt((((diff/1000/60/60/24 - days)*24 - hours)*60 - minutes)*60);
     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
 };
-//console.log(getDiff(new Date(2019,2,5,4,30,20), new Date(2019,2,20,3,25,12)));
+//console.log(getDiff(new Date(2019,0,0,0,0,0), new Date(2019,0,1,2,15,10)));
