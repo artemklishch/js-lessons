51fbf43e3875d9a593f03689c57c05c0f54210c5
Artem <artemklishch@gmail.com>
2019-12-16 10:06:01 +0200

    task3

--- lesson24/task3/index.js
+++ lesson24/task3/index.js
@@ -15,14 +15,14 @@ export const getDiff = (startDate, endDate) => {
 
 
     const deff = Math.abs(endDate - startDate);
-    const days = Math.trunc(deff/1000/60/60/24);
+    const days = parseInt(deff/1000/60/60/24);
     let tempDeffVal = deff/1000/60/60/24;
-    const hours = Math.trunc((tempDeffVal - days)*60);
+    const hours = parseInt((tempDeffVal - days)*60);
     tempDeffVal = (tempDeffVal - days)*60;
-    const minutes = Math.trunc((tempDeffVal - hours)*60);
+    const minutes = parseInt((tempDeffVal - hours)*60);
     tempDeffVal = (tempDeffVal - hours)*60;
-    const seconds = Math.trunc(((tempDeffVal*60) - minutes)*100);
+    const seconds = parseInt(((tempDeffVal*60) - minutes)*100);
     return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 
 };
-//console.log(getDiff(new Date(2019,2,5,4,30,20), new Date(2019,2,20,3,25,12)));
+console.log(getDiff(new Date(2019,2,5,4,30,20), new Date(2019,2,20,3,25,12)));
