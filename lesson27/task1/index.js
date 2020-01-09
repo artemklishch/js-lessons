d707ac3300632f04d16a4dda2c1f073eecbc3739
Artem <artemklishch@gmail.com>
2020-01-09 10:07:04 +0200

    df

---
+++ lesson27/task1/index.js
@@ -0,0 +1,7 @@
+//getLocalSrorageData
+
+localStorage.setItem('name','Tom');
+localStorage.clear();
+localStorage.setItem('hobbies', {name: 'Tom',});
+
+console.log(localStorage.getItem('hobbies'));
