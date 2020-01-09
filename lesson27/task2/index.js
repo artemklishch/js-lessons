315ee87f0806cd1b3f662e3f731f068499418d03
Artem <artemklishch@gmail.com>
2020-01-09 15:53:23 +0200

    l27t2

---
+++ lesson27/task2/index.js
@@ -0,0 +1,30 @@
+const counterElem = document.querySelector('.counter');
+const counterValueElem = document.querySelector('.counter__value');
+
+const onCounterChange = e => {
+    const isButton = e.target.classList.contains('counter__button');
+    if(!isButton){
+        return;
+    }
+    const action = e.target.dataset.action;
+    const oldValue = Number(counterValueElem.textContent);
+    const newValue = action === 'decrease' 
+        ? oldValue - 1
+        : oldValue + 1;
+    
+    localStorage.setItem('counterElem', newValue);
+
+    counterValueElem.textContent = newValue
+    
+};
+counterElem.addEventListener('click', onCounterChange);
+
+const onStorageChange = e => {
+    counterValueElem.textContent = e.newValue;
+};
+window.addEventListener('storage', onStorageChange);
+
+const onDocumentLoaded = () => {
+    counterValueElem.textContent = localStorage.getItem('counterElem') || 0;
+};
+document.addEventListener('DOMContentLoaded', onDocumentLoaded);
