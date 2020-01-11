1b1bb93c4276545360fae4e802af7bea608fc083
Artem <artemklishch@gmail.com>
2020-01-11 20:11:05 +0200

    t2l28

--- lesson28/task1/index.js
+++ lesson28/task1/index.js
@@ -1,11 +1,11 @@
-// export const markFavorites = (tree, favorites) => {
-//     const isFavorite = favorites.includes(tree.id);
-//     return {
-//         ...tree,
-//         isFavorite,
-//         nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
-//     };
-// };
+export const markFavorites = (tree, favorites) => {
+    const isFavorite = favorites.includes(tree.id);
+    return {
+        ...tree,
+        isFavorite,
+        nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
+    };
+};
 
 
 // const tree = {
@@ -32,42 +32,4 @@
 // };
 // const favorites = ['id-6', 'id-17'];
 // const result = markFavorites(tree,favorites);
-// console.log(result);
-
-
-
-//  const favorites = ['id-6', 'id-17'];
-// const tree = {
-// 	id: 'id-1',
-// 	name: 'Products',
-// 	//idFavorite: true,
-// 	nodes: [
-// 		{
-// 			id:'id-2',
-// 			name: 'Food',
-// 			nodes: [
-// 				{
-// 					id: 'id-6',
-// 					name: 'Drinks',
-// 					nodes: [],
-// 				},
-// 			],
-// 		},
-// 		{
-// 			id: 'id-17',
-// 			name: 'Vehicles',
-// 			nodes: [],
-// 		},
-// 	],
-// };
-
-const markFavourites = (tree, favorites) => {
-	const isFavorite = favorites.includes(tree.id);
-	return {
-		...tree,
-		isFavorite,
-		nodes: tree.nodes.map(childNode => markFavourites(childNode, favorites)),
-	};
-};
-// const result = markFavourites(tree, favorites);
 // console.log(result);
