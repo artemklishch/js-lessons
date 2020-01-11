caab871d5e52140e02c493da4629bab06e86da43
Artem <artemklishch@gmail.com>
2020-01-11 20:08:20 +0200

    df

--- lesson28/task1/index.js
+++ lesson28/task1/index.js
@@ -1,4 +1,4 @@
-export function markFavorites(tree, favorites){
+export const markFavorites = (tree, favorites) => {
     const isFavorite = favorites.includes(tree.id);
     return {
         ...tree,
@@ -8,28 +8,28 @@ export function markFavorites(tree, favorites){
 };
 
 
-// const tree = {
-// 	id: 'id-1',
-// 	name: 'Products',
-// 	nodes: [
-// 		{
-// 			id:'id-2',
-// 			name: 'Food',
-// 			nodes: [
-// 				{
-// 					id: 'id-6',
-// 					name: 'Drinks',
-// 					nodes: [],
-// 				},
-// 			],
-// 		},
-// 		{
-// 			id: 'id-17',
-// 			name: 'Vehicles',
-// 			nodes: [],
-// 		},
-// 	],
-// };
-// const favorites = ['id-6', 'id-17'];
-// const result = markFavorites(tree,favorites);
-// console.log(result);
+const tree = {
+	id: 'id-1',
+	name: 'Products',
+	nodes: [
+		{
+			id:'id-2',
+			name: 'Food',
+			nodes: [
+				{
+					id: 'id-6',
+					name: 'Drinks',
+					nodes: [],
+				},
+			],
+		},
+		{
+			id: 'id-17',
+			name: 'Vehicles',
+			nodes: [],
+		},
+	],
+};
+const favorites = ['id-6', 'id-17'];
+const result = markFavorites(tree,favorites);
+console.log(result);
