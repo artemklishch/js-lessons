b0ca499d58f65f101e8235593df6918166f48134
Artem <artemklishch@gmail.com>
2020-01-11 20:04:10 +0200

    l28t2

---
+++ lesson28/task2/index.js
@@ -0,0 +1,25 @@
+export const calc = value => {
+    let result = value;
+    const calculator = {
+        add(number){
+            result += number;
+            return this;
+        },
+        subtract(number){
+            result -= number;
+            return this;
+        },
+        mult(number){
+            result *= number;
+            return this;
+        },
+        div(number){
+            result /= number;
+            return this;
+        },
+        result(){
+            return result;
+        },
+    };
+    return calculator;
+};
