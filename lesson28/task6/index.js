5156e64ddd54756a48a774d3037413d3c8248002
Artem <artemklishch@gmail.com>
2020-01-13 17:46:29 +0200

    fib

--- lesson28/task6/index.js
+++ lesson28/task6/index.js
@@ -23,18 +23,43 @@
 // const result = maxFibonacci(15).fibFunc().result();
 // console.log(result);
 
-export const maxFibonacci = (n) => {
-    let a = 1;
-    let b = 1;
-    for (let i = 3; i <= n; i++) {
-        if(b >= n){
-            b = a;
-            break;
-        } 
-      let c = a + b;
-      a = b;
-      b = c;
-    }
-    return b;
-  }
+// export const maxFibonacci = (n) => {
+//     let a = 1;
+//     let b = 1;
+//     for (let i = 3; i <= n; i++) {
+//         if(b >= n){
+//             b = a;
+//             break;
+//         } 
+//       let c = a + b;
+//       a = b;
+//       b = c;
+//     }
+//     return b;
+//   }
   //console.log(maxFibonacci(35));
+
+
+
+
+//maxFibonacci(15) // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+const fibFunc = (num) => {
+  let arr = [0,1];
+  let a = 0;
+  let b = 1;
+  for (let i = 3; i <= num; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+    arr.push(c);
+  }
+  return arr;
+};
+export const maxFibonacci = func => value => {
+  let arr = func(value).reverse();
+  return arr.find(elem => elem < value);
+};
+
+const findMaxFibNumber = maxFibonacci(fibFunc);
+console.log(findMaxFibNumber(15));
