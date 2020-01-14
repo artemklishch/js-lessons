b86ab7150504e916319b1f8bee39398e47256204
Artem <artemklishch@gmail.com>
2020-01-14 11:45:56 +0200

    er

--- lesson28/task6/index.js
+++ lesson28/task6/index.js
@@ -15,26 +15,6 @@ export const maxFibonacci = value => {
         
     }
 }; 
-// console.log(maxFibonacci(22));
+//console.log(maxFibonacci(9));
 
 
-//maxFibonacci(15) // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
-// const fibFunc = (num) => {
-//   let arr = [0,1];
-//   let a = 0;
-//   let b = 1;
-//   for (let i = 3; i <= num; i++) {
-//     let c = a + b;
-//     a = b;
-//     b = c;
-//     arr.push(c);
-//   }
-//   return arr;
-// };
-// export const maxFibonacci = func => value => {
-//   let arr = func(value).reverse();
-//   return arr.find(elem => elem < value);
-// };
-
-// const findMaxFibNumber = maxFibonacci(fibFunc);
-// console.log(findMaxFibNumber(0.1));
