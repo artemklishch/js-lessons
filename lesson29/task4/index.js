e435d91b6ce473e56bd6b140b9387a16f01d0462
Artem <artemklishch@gmail.com>
2020-01-14 23:44:00 +0200

    er

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -1,19 +1,19 @@
 export const requestUserData = (userId, callback) => {
-    const obj = {
-        name: 'Tom',
-        age: 18,
-        userId:userId,
-        email:'ed@df.com'
-    };
+    
     if(userId !== 'broken'){
-        setTimeout(() => callback(null,obj), 3000);
+        setTimeout(() => callback(null, {
+            name: 'Tom',
+            age: 18,
+            userId:userId,
+            email:'ed@df.com'
+        }), 3000);
     }else if(userId === 'broken'){
         setTimeout(() => callback(null,'Failed to load user data'), 3000);
     }
     
 };
-const onFormUserData = (error, data) => data;
-requestUserData('userid777', onFormUserData);
+//const onFormUserData = (error, data) => data;
+//requestUserData('broken', onFormUserData);
 
 
 
