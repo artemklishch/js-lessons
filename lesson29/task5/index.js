d60146bb545664b42813a263b58fac49e58a75cc
Artem <artemklishch@gmail.com>
2020-01-15 15:14:06 +0200

    ret

--- lesson29/task5/index.js
+++ lesson29/task5/index.js
@@ -1,18 +1,16 @@
-export const delay = (delay, callback, context) => {
-    return function(...args){
-        setTimeout(() => callback.apply(context, args), delay);
-    }
-    
+export const delay = (delay, callback, context, ...args) => {
+    setTimeout(function(){
+        callback(context, args)
+    }, delay);
 };
 
 // const object = {
-//     bame: 'Tom',
+//     name: 'Tom',
 //     age: 27,
 // };
 
 // const someCallback = (obj) => {
 //     console.log(obj);
 // };
-// const a = delay(3000, someCallback, object);
-// a(object.place = 'hjhj');
+// delay(3000, someCallback, object);
 
