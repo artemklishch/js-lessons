694febe90c8c1985b65dd2103b2d9314b0bb7fda
Artem <artemklishch@gmail.com>
2020-01-15 11:22:30 +0200

    dfs

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -22,7 +22,7 @@ export const requestUserData = (userId, callback) => {
         email:`${userId}@example.com`
     }), timeSeconds);
 };
-const onFormUserData = (error, data) => data;
+//const onFormUserData = (error, data) => data;
 
 // const onFormUserData = (error, data) => {
 //     if(error){
