ab9de39a4a4c05bc86e08c820c786e884224855c
Artem <artemklishch@gmail.com>
2020-01-15 22:33:12 +0200

    fdg

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -3,19 +3,20 @@ export const addImage = imgSrc => {
 			const imgElem = document.createElement("img");
 			imgElem.setAttribute('alt', 'My Photo');
 			imgElem.src = imgSrc;
-            imgElem.width = 200;
-            imgElem.height = 100;
-
+            
 			const containerElem = document.querySelector('.page');
 			containerElem.append(imgElem);
 
 			const onImageLoaded = () => {
-				const { width, height } = imgElem;
-				resolve({ width, height });
+                const { width, height } = imgElem;
+                if(width === 200 && height === 100){
+                    resolve({ width, height });
+                }else reject(new Error('Image load failed'));
+				
 			};
 			imgElem.addEventListener('load', onImageLoaded);
 
-			imgElem.addEventListener('error', () => resolve(new Error('Image load failed')));
+			//imgElem.addEventListener('error', () => reject(new Error('Image load failed')));
 		});
 	return p;
 };
