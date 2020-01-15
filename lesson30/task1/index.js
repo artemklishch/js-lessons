d129eb32fd02ddc90e78b4d77bb29d3f39f512c1
Artem <artemklishch@gmail.com>
2020-01-15 22:09:57 +0200

    t1l30

---
+++ lesson30/task1/index.js
@@ -0,0 +1,24 @@
+export const addImage = imgSrc => {
+	const p = new Promise((resolveCb, rejectCb) => {
+			const imgElem = document.createElement("img");
+			imgElem.setAttribute('alt', 'My Photo');
+			imgElem.src = imgSrc;
+
+			const containerElem = document.querySelector('.page');
+			containerElem.append(imgElem);
+
+			const onImageLoaded = () => {
+				const { width, height } = imgElem;
+				resolveCb({ width, height });
+			};
+			imgElem.addEventListener('load', onImageLoaded);
+
+			imgElem.addEventListener('error', () => rejectCb(new Error('Image load failed')));
+		});
+	return p;
+};
+
+// const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+// const resultPromise = addImageWithPromise(imgSrc);
+// resultPromise.then(data => console.log(data));
+// resultPromise.catch(error => console.log(error));
