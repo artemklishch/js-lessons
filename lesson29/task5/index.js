4a76e525286db7fa79554b3a1fd746c9297634be
Artem <artemklishch@gmail.com>
2020-01-15 12:58:54 +0200

    dsf

--- lesson29/task5/index.js
+++ lesson29/task5/index.js
@@ -1,5 +1,5 @@
 export const delay = (delay, callback, context, ...args) => {
-    setTimeout(() => callback.call(this, context,args), delay);
+    setTimeout(() => callback.call(this, context, args), delay);
 };
 
 // const object = {
@@ -11,3 +11,4 @@ export const delay = (delay, callback, context, ...args) => {
 //     console.log(obj);
 // };
 // delay(3000, someCallback, object, object.place = 'hjhj');
+
