d2923a59d296a87b9f5cce30a010adaa3d836c3f
Artem <artemklishch@gmail.com>
2020-01-15 23:18:54 +0200

    ert

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -1,10 +1,9 @@
-export const addImageV2 = imgSrc => {
+export const addImage = imgSrc => {
 	const p = new Promise((resolve, reject) => {
 			const imgElem = document.createElement("img");
 			imgElem.setAttribute('alt', 'My Photo');
             imgElem.src = imgSrc;
-            imgElem.width = 200;
-            imgElem.height = 100;
+
 			const containerElem = document.querySelector('.page');
 			containerElem.append(imgElem);
 
@@ -20,7 +19,6 @@ export const addImageV2 = imgSrc => {
 };
 
 //const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
-const imgSrc = 'https://server.com/image.png';
-const resultPromise = addImageV2(imgSrc);
-resultPromise.then(data => console.log(data));
-resultPromise.catch(error => console.log(error));
+// const resultPromise = addImage(imgSrc);
+// resultPromise.then(data => console.log(data));
+// resultPromise.catch(error => console.log(error));
