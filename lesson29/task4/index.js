331c31510f7ba0970338536ca87f0211cd620864
Artem <artemklishch@gmail.com>
2020-01-15 10:37:54 +0200

    er

--- lesson29/task4/index.js
+++ lesson29/task4/index.js
@@ -1,19 +1,34 @@
+// const getRandomNumbers = (length, min, max) => {
+//     if(max < min) return null;
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     if(max - min < 0)return null;
+//     return Array(length).fill()
+//     .map(() => Math.floor(Math.random()*(max-min+1)+min));
+// };
+const getRandomNumber = (min, max) => max < min
+    ? null :Math.floor(Math.random()*(max-min+1)+min);
+
 export const requestUserData = (userId, callback) => {
-    
-    if(userId !== 'broken'){
-        setTimeout(() => callback(null, {
-            name: 'Tom',
-            age: 18,
-            userId:userId,
-            email:'ed@df.com'
-        }), 3000);
-    }else if(userId === 'broken'){
-        setTimeout(() => callback(null,'Failed to load user data'), 3000);
+    const randomNumber = getRandomNumber(1,3);
+    if(userId === 'broken'){
+        setTimeout(() => callback(null,'Failed to load user data'),
+            randomNumber * 1000);
+        return;
     }
-    
+    setTimeout(() => callback(null, {
+        name: 'Tom',
+        age: 18,
+        userId: userId,
+        email:'ed@df.com'
+    }), randomNumber * 1000);
 };
-//const onFormUserData = (error, data) => data;
-//requestUserData('broken', onFormUserData);
+const onFormUserData = (error, data) => data;
+//requestUserData('sdfsdfsdf', onFormUserData);
+
+
+
+
 
 
 
