15798ce23afa9b805b3e31b1757764fac280176f
Artem <artemklishch@gmail.com>
2020-01-16 13:25:34 +0200

    et

--- lesson30/task3/addImage.js
+++ lesson30/task3/addImage.js
@@ -1,10 +1,23 @@
-export function addImage(imgSrc){
-    const imgElem = document.createElement("img");
-    imgElem.setAttribute('alt', 'My Photo');
-    imgElem.src = imgSrc;
+const onImageL = (error, data) => {
+    if(error) return;
+    const { width, height } = data;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} x ${height}`;
+};
 
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
+export const addImage = (imgSrc) => {
+	const imgElem = document.createElement("img");
+	imgElem.setAttribute('alt', 'My Photo');
+	imgElem.src = imgSrc;
+
+	const containerElem = document.querySelector('.page');
+	containerElem.append(imgElem);
+
+    const onImageLoaded = () => onImageL(null, { width: 200, height: 100 });
+    imgElem.addEventListener('load', onImageLoaded);
     
+    imgElem.addEventListener('error', () => onImageL('Image load failed'));
+
     return imgElem;
+
 };
