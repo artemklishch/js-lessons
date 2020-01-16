857ffbaa320a4d9c3ee91c5f45beaf2b85b3b707
Artem <artemklishch@gmail.com>
2020-01-16 14:47:47 +0200

    rty

--- lesson30/task3/index.js
+++ lesson30/task3/index.js
@@ -1,9 +1,23 @@
-import { addImage } from './addImage.js';
+//import { addImage } from './addImage.js';
 
-export const addImageV2 = imgSrc => {
-    return addImage(imgSrc);
+// export const addImageV2 = imgSrc => {
+//     return addImage(imgSrc);
+// };
+export const addImageV2 = (imgSrc) => {
+    return new Promise((resolve,reject) => {
+        const imgElem = document.createElement("img");
+        imgElem.setAttribute('alt', 'My Photo');
+        imgElem.src = imgSrc;
+
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imgElem);
+
+        const onImageLoaded = () => resolve({ width: 200, height: 100 });
+        imgElem.addEventListener('load', onImageLoaded);
+        
+        imgElem.addEventListener('error', () => reject('Image load failed'));
+    });
 };
-
 
 const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
 
