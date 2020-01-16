1cbba9da03a67a17f4101ade424bd82db7e48b99
Artem <artemklishch@gmail.com>
2020-01-16 14:52:07 +0200

    ert

--- lesson30/task3/index.js
+++ lesson30/task3/index.js
@@ -5,7 +5,7 @@ export const addImageV2 = imgSrc => {
 };
 
 
-const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
+//const imgSrc = 'https://gromcode.s3.eu-central-1.amazonaws.com/front-end/html-css/lesson15/task1/big.jpeg';
 
 addImageV2(imgSrc)
     .then(data => console.log(data))
