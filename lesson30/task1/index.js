aefe5e5aaecdb8e9ba6c6a343cfe057487bd9600
Artem <artemklishch@gmail.com>
2020-01-16 10:26:43 +0200

    dfg

--- lesson30/task1/index.js
+++ lesson30/task1/index.js
@@ -13,7 +13,7 @@ export const addImage = imgSrc => {
 			};
 			imgElem.addEventListener('load', onImageLoaded);
 
-			imgElem.addEventListener('error', () => reject(new Error('Image load failed')));
+			imgElem.addEventListener('error', reject('Image load failed'));
 		});
 	return p;
 };
