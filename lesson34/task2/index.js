73499ee038d430679ed3fd2bb6c3a64f5b9cc272
Artem <artemklishch@gmail.com>
2020-01-23 17:01:24 +0200

    ewr

--- lesson34/task2/index.js
+++ lesson34/task2/index.js
@@ -64,7 +64,7 @@ const onFormSubmit = event => {
     event.preventDefault();
     const formData = [...new FormData(formElem)]
         .reduce((acc,[field,value]) => ({...acc, [field]:value}),{});
-    const email = formData.email;
+    //const email = formData.email;
     return fetch(baseUrl, {
         method: 'POST',
         headers: {
