f9262c4ac814dd8468e2c64065c411368c4c8813
Artem <artemklishch@gmail.com>
2020-01-25 16:27:40 +0200

    t3l36

--- lesson36/task3/index.js
+++ lesson36/task3/index.js
@@ -1,18 +1,39 @@
+// export const getUserBlogs = async arrayOfUsers => {
+//         const pr = await new Promise((resolve,reject) => {
+//             const arr = [];
+//             if(arrayOfUsers.length === 0){
+//                 reject(new Error('There aren`t elements'));
+//             }
+//             arrayOfUsers.forEach(elem => {
+//                 fetch(`https://api.github.com/users/${elem}`)
+//                     .then(response => response.json())
+//                     .then(res => {
+//                         arr.push(res.blog);
+//                     });   
+//             });
+//             resolve(arr);     
+//         });
+//         return pr;
+// };
+
 export const getUserBlogs = async arrayOfUsers => {
-        const pr = await new Promise((resolve,reject) => {
-            const arr = [];
-            if(arrayOfUsers.length === 0){
-                reject(new Error('There aren`t elements'));
-            }
-            arrayOfUsers.forEach(elem => {
-                fetch(`https://api.github.com/users/${elem}`)
-                    .then(response => response.json())
-                    .then(res => {
-                        arr.push(res.blog);
-                    });   
-            });
-            resolve(arr);     
+    const pr = await Promise.all(
+        arrayOfUsers.map(async element => element = await fetch(`https://api.github.com/users/${element}`))        
+    )
+    .then(async arrayOfPromises => {
+        const arr = [];
+        arrayOfPromises.forEach(async elem => {
+            const json = await elem.json();
+            arr.push(json.blog);
         });
-        return pr;
+        return arr;
+    });
+    return pr;
 };
-// getUserBlogs(['google', 'facebook', 'gaearon']);
+// getUserBlogs(['google', 'facebook', 'gaearon'])
+//     .then(res => console.log(res));
+    
+
+   
+
+    
