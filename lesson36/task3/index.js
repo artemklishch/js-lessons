ba8e79215dbf95cb3ccf978325fbd94e485f8fca
Artem <artemklishch@gmail.com>
2020-01-25 13:43:27 +0200

    t3l36

--- lesson36/task3/index.js
+++ lesson36/task3/index.js
@@ -1,19 +1,18 @@
 export const getUserBlogs = async arrayOfUsers => {
         const pr = await new Promise((resolve,reject) => {
             const arr = [];
+            if(arrayOfUsers.length === 0){
+                reject(new Error('There aren`t elements'));
+            }
             arrayOfUsers.forEach(elem => {
                 fetch(`https://api.github.com/users/${elem}`)
                     .then(response => response.json())
                     .then(res => {
-                        return arr.push(res.blog);
+                        arr.push(res.blog);
                     });   
-            });     
-            if(arr.length === 0){
-                resolve(arr);
-            }else reject(new Error('There aren`t elements'));
+            });
+            resolve(arr);     
         });
         return pr;
 };
 // getUserBlogs(['google', 'facebook', 'gaearon']);
-    
-
