29d690d8b32e7d3eb47cf61fb9aa3f010ea6a575
Artem <artemklishch@gmail.com>
2020-01-27 13:51:34 +0200

    dfg

--- lesson36/task3/index.js
+++ lesson36/task3/index.js
@@ -1,13 +1,16 @@
 export const getUserBlogs = async arrayOfUsers => {
-    const pr = await Promise.all(
-        arrayOfUsers.map(element => element = fetch(`https://api.github.com/users/${element}`))
-    );
-    const arr = [];
-    pr.forEach(async elem => {
-        const { blog } = await elem.json(); 
-        arr.push(blog);
+    return await Promise.all(
+        arrayOfUsers.map(element => 
+            element = fetch(`https://api.github.com/users/${element}`))
+    )
+    .then(array => {
+        const arr = [];
+        array.forEach(async elem => {
+                const a = await elem.json();
+                arr.push(a.blog);
+        });
+       return arr;
     });
-    return arr;
 };
-getUserBlogs(['google', 'facebook', 'gaearon'])
-    .then(res => console.log(res));    
+// getUserBlogs(['google', 'facebook', 'gaearon'])
+//     .then(res => console.log(res));    
