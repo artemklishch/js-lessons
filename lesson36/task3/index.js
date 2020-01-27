0abcd2794fdcb87fd955e703d28f19d11ebb3bd9
Artem <artemklishch@gmail.com>
2020-01-27 16:56:23 +0200

    546

--- lesson36/task3/index.js
+++ lesson36/task3/index.js
@@ -1,14 +1,19 @@
 export const getUsersBlogs = async arrayOfUsers => {
-    const arrayOfPromises = arrayOfUsers
+    try{
+        const arrayOfPromises = arrayOfUsers
         .map(userId => {
-            const responsePromise = fetch(`https://api.github.com/users/${userId}`);
+            const responsePromise = fetch(`https://api.github.com/user/${userId}`);
             const userDataPromise = responsePromise
                 .then(response => response.json());
             return userDataPromise;
         });
-    const usersData = await Promise.all(arrayOfPromises);
-    const urlData = usersData.map(elem => elem = elem.blog);
-    return urlData;
+        const usersData = await Promise.all(arrayOfPromises);
+        const urlData = usersData.map(elem => elem = elem.blog);
+        return urlData;
+    }catch(err){
+        throw new Error(`Failed to load data`);
+    }
+    
 };
 // getUsersBlogs(['google', 'facebook', 'gaearon'])
 //     .then(res => console.log(res));    
