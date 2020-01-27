1710dc7f07aceb838b1ffbb4daf0bd6911df12ac
Artem <artemklishch@gmail.com>
2020-01-27 10:52:13 +0200

    rt

--- lesson36/task3/index.js
+++ lesson36/task3/index.js
@@ -1,34 +1,9 @@
-// export const getUserBlogs = async arrayOfUsers => {
-//         return await Promise.all(
-//             arrayOfUsers.map(async element => element = await fetch(`https://api.github.com/users/${element}`))        
-//         )
-//         .then(async arrayOfPromises => {
-//             const arr = [];
-//             arrayOfPromises.forEach(async elem => {
-//                 const json = await elem.json();
-//                 arr.push(json.blog);
-//             });
-//             return arr;
-//         });
-// };
+export const getUserBlogs = async arrayOfUsers => {
+        return await Promise.all(
+            arrayOfUsers.map(element => element = fetch(`https://api.github.com/users/${element}`)
+                .then(response => response.json())
+                .then(elem => elem = elem.blog))        
+        );
+};
 // getUserBlogs(['google', 'facebook', 'gaearon'])
 //     .then(res => console.log(res));    
-
-
-
-
-export const getUserBlogs = async arrayOfUsers => {
-    return await Promise.all(
-        arrayOfUsers.map(async element => element = await fetch(`https://api.github.com/users/${element}`))        
-    )
-    .then(async arrayOfPromises => {
-        arrayOfPromises.map(async (elem,index) => {
-            const json = await elem.json();
-            const value = await json.blog;
-            arrayOfPromises.splice(index,1,value);
-        });
-        return arrayOfPromises;
-    });
-};
-getUserBlogs(['google', 'facebook', 'gaearon'])
-    .then(res => console.log(res));    
